fb724297ed8b4392d5574b0e1531dd09da103822
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2021-09-04 18:49:26 -0300

    add insomnia endpoints

--- src/api/controllers/lancamentos/LancamentosController.ts
+++ src/api/controllers/lancamentos/LancamentosController.ts
@@ -1,14 +1,21 @@
 import { lancamento, usuario } from "@prisma/client";
 import { IRouterContext } from "koa-router";
-import { savelaunch } from "./repositories/save-user-repository";
+import { savelaunch } from "./repositories/save-lancamento-repository";
 
 export const store = async (ctx: IRouterContext) => {
 
-  const lancamento: lancamento = ctx.request.body
+  const lancamento: lancamento = ctx.request.body;
 
   try {
 
     const lancamentoSalvo = await savelaunch(lancamento);
+
+    if(!lancamentoSalvo){
+      ctx.status = 404;
+      ctx.body = {message: "Usuário não encontrado."};
+      return;
+    };
+
     ctx.body = lancamentoSalvo;
 
   } catch (error) {
@@ -16,6 +23,5 @@ export const store = async (ctx: IRouterContext) => {
     ctx.status = 500;
     ctx.body = { error };
 
-  }
-
-}
+  };
+};
--- src/api/controllers/lancamentos/repositories/save-lancamento-repository.ts
+++ src/api/controllers/lancamentos/repositories/save-lancamento-repository.ts
@@ -2,27 +2,28 @@ import { lancamento, PrismaClient, usuario } from "@prisma/client";
 import { Console } from "console";
 import { resolvePtr } from "dns";
 import { ServerResponse } from "http";
+import { IRouterContext } from "koa-router";
 import { json } from "stream/consumers";
 
 const prisma = new PrismaClient();
 
 export const savelaunch = async (lancamento: lancamento) => {
 
-  const { id_usuario, valor, descricao, data } = lancamento
+  const { id_usuario, valor, descricao, data } = lancamento;
+
+  const id_usuario_existe: boolean = false;
 
   try {
 
-    const usuario = ""
-    // await prisma.usuario.findFirst({
-    //   where: {
-    //     id: id_usuario as number,
-    //   }
-    // });
+    const usuario = await prisma.usuario.findFirst({
+      where: {
+        id: id_usuario as string,
+      }
+    });
 
-    if (!usuario) {
-      throw new Error('tetse');
+    if(!usuario){
+      return id_usuario_existe;
     }
-
 
     const lancamento = await prisma.lancamento.create({
       data: {
