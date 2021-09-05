95615a1ccc82289ea0b72be5e1098b907ce00ab9
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2021-09-04 23:32:43 -0300

    add incriptation in password

--- src/api/controllers/usuarios/repositories/save-user-repository.ts
+++ src/api/controllers/usuarios/repositories/save-user-repository.ts
@@ -1,17 +1,21 @@
 import { PrismaClient, usuario } from "@prisma/client";
+import { hash } from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
 export const saveUser = async (usuario: usuario) => {
 
-  const { email, nome } = usuario;
+  const { email, nome, password } = usuario;
+
+  const passwordHash = await hash(password,8);
 
   try {
 
     const usuarios = await prisma.usuario.create({
       data: {
         email: email,
-        nome: nome
+        nome: nome,
+        password: passwordHash
       }
     });
 
